e75eaeaebb29d736b043850a6eed2bded858dbd6
lorenzo.canovas <lorenzo.canovas@mi.unc.edu.ar>
2025-09-18 17:59:08 -0300

    ING-11 modifying main and App to add the created page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { useEffect, useMemo } from 'react'
-import { createHttpService } from './services/httpService';
+import { Link } from 'react-router'
+
+import { createHttpService } from './services/httpService'
+
+import { FRONTEND_PATHS } from './constants/frontendPaths'
 
 function App() {
-  const httpService = useMemo(() => createHttpService(), []);
+	const httpService = useMemo(() => createHttpService(), [])
 
-  useEffect(() => {
-    if (!httpService) return;
+	useEffect(() => {
+		if (!httpService) return
 
-    console.log("HTTP Service initialized:", httpService);
-  }, [httpService]);
+		console.log('HTTP Service initialized:', httpService)
+	}, [httpService])
 
-  return (
-    <div>Here comes our page content.</div>
-  )
+	return (
+		<div>
+			<h1>Here comes our page content.</h1>
+			<div className='min-h-screen flex justify-center items-center'>
+				<Link to={FRONTEND_PATHS.MATCH_CREATE} className='bg-white p-2 rounded-lg'>
+					Create match (luego se elimina esto)
+				</Link>
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,22 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 import './index.css'
+
 import App from './App.tsx'
+import MainLayout from './containers/MainLayout'
+import CreateMachPage from './containers/CreateMachPage.tsx'
+
+import { FRONTEND_PATHS } from './constants/frontendPaths.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path={FRONTEND_PATHS.HOME} element={<App />} />
+				<Route element={<MainLayout />}>
+					<Route path={FRONTEND_PATHS.MATCH_CREATE} element={<CreateMachPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
 )
