30742e579de44893faf12fe51168af35416d83f4
Tomas Castrillon <tomas.castrillon@mi.unc.edu.ar>
2025-09-20 15:36:24 -0300

    ING-21 structural corrections

--- src/containers/game/components/DrawPile.tsx
+++ src/containers/game/components/DrawPile.tsx
@@ -4,15 +4,11 @@ type DrawPileProps = {
   cardCount: number;
 };
 
-function DrawPile({ cardCount }: DrawPileProps) {
+export default function DrawPile({ cardCount }: DrawPileProps) {
   return (
-    <>
-      <div className="w-40 h-50 position absolute top-90 left-235">
-        <img src={cardBack["CARD_BACK"]} />
-        <div className="draw-pile text-white">REMAINING: {cardCount}</div>
-      </div>
-    </>
+    <div className="w-40 h-50 position absolute top-90 left-235">
+      <img src={cardBack["CARD_BACK"]} />
+        <div className="text-white">REMAINING: {cardCount}</div>
+    </div>
   )
 }
-
-export default DrawPile;
