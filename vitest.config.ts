a9d8857f93a171d1f09a5c78ab388a7a684f14b0
lorenzo.canovas <lorenzo.canovas@mi.unc.edu.ar>
2025-09-21 23:38:19 -0300

    ING-11 the problem with aliases in tests is solved

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  }
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
 })
