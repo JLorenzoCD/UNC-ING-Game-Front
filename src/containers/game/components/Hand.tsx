b18723f7eb0464e7773005759b53886728f5d369
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-09-22 18:42:18 -0300

    ING-19 remove `null`-filling logic and improve card typing to allow same-kind cards to coexist in the same hand

--- src/containers/game/components/Hand.tsx
+++ src/containers/game/components/Hand.tsx
@@ -1,11 +1,10 @@
-import type { Card as CardSchema } from "@/types/card";
+import type { HandCard } from "@/types/card";
 import Card from "./Card";
-import { useMemo } from "react";
 
 export const HAND_SIZE = 6; // Número máximo (y necesario al princio de cada ronda) de cartas en la mano
 
 interface HandProps {
-  cards: CardSchema[];
+  cards: HandCard[];
 }
 
 function EmptyHandPosition() {
@@ -19,20 +18,9 @@ function EmptyHandPosition() {
 }
 
 export default function Hand({ cards }: HandProps) {
-  const hand: Array<CardSchema | null> = useMemo(() => {
-    if (cards.length < HAND_SIZE) {
-      console.warn(`Hand has less than ${HAND_SIZE} cards. Filling with empty slots.`);
-
-      // Llenamos la mano con posiciones vacías hasta llegar al tamaño HAND_SIZE.
-      return [...cards, ...Array.from({ length: HAND_SIZE - cards.length }, () => null)];
-    }
-
-    return cards;
-  }, [cards])
-
   return (
     <div data-testid="hand" className="flex gap-x-4 items-center">
-      {hand.map((card) => 
+      {cards.map((card) => 
         card === null
           ? <EmptyHandPosition key={`empty-${Math.random()}`} />
           : <Card key={card.id} name={card.name} description={card.description} />
