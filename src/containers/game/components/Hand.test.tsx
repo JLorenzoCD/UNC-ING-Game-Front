133c1b8af5c41000f1c8766a863c88b5f93a2455
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-09-24 18:42:58 -0300

    ING-36 extract mock data out from the test suite

--- src/containers/game/components/Hand.test.tsx
+++ src/containers/game/components/Hand.test.tsx
@@ -4,74 +4,78 @@ import { beforeEach, describe, expect, it, vi } from "vitest";
 import type { HandCard } from "@/types/card";
 import Hand from "./Hand";
 
+const match_id = crypto.randomUUID()
+const player_id = crypto.randomUUID()
+
+const fullHand: HandCard[] = [
+  {
+    id: crypto.randomUUID(),
+    card_id: crypto.randomUUID(),
+    match_id,
+    player_id,
+    name: "POIROT",
+    description: "Un detective belga famoso por su intelecto y sus métodos poco convencionales.",
+    is_discarded: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    card_id: crypto.randomUUID(),
+    match_id,
+    player_id,
+    name: "MARPLE",
+    description: "Una astuta anciana que resuelve misterios en su pequeño pueblo.",
+    is_discarded: false
+  },
+  {
+    id: crypto.randomUUID(),
+    card_id: crypto.randomUUID(),
+    match_id,
+    player_id,
+    name: "SATTERTHWAITE",
+    description: "Un hombre modesto con una habilidad sorprendente para resolver crímenes.",
+    is_discarded: false
+  },
+  {
+    id: crypto.randomUUID(),
+    card_id: crypto.randomUUID(),
+    match_id,
+    player_id,
+    name: "PYNE",
+    description: "Un detective privado con un enfoque pragmático para resolver casos.",
+    is_discarded: false
+  },
+  {
+    id: crypto.randomUUID(),
+    card_id: crypto.randomUUID(),
+    match_id,
+    player_id,
+    name: "BRENT",
+    description: "Un detective aficionado con un talento natural para la observación.",
+    is_discarded: false,
+    
+  },
+  {
+    id: crypto.randomUUID(),
+    card_id: crypto.randomUUID(),
+    match_id,
+    player_id,
+    name: "TOMMY",
+    description: "Un joven detective que trabaja junto a su esposa Tuppence.",
+    is_discarded: false
+  },
+]
+
+const partialHand = [fullHand[0], null, fullHand[1], null, fullHand[2], fullHand[3]]; // 4 cartas, 2 espacios vacíos en medio
+
+const emptyHand = [null, null, null, null, null, null]; // 0 cartas, 6 espacios vacíos
+
 describe("Hand", () => {
-  const match_id = crypto.randomUUID()
-  const player_id = crypto.randomUUID()
-
-  const fullHand: HandCard[] = [
-    {
-      id: crypto.randomUUID(),
-      card_id: crypto.randomUUID(),
-      match_id,
-      player_id,
-      name: "POIROT",
-      description: "Un detective belga famoso por su intelecto y sus métodos poco convencionales.",
-      is_discarded: false,
-    },
-    {
-      id: crypto.randomUUID(),
-      card_id: crypto.randomUUID(),
-      match_id,
-      player_id,
-      name: "MARPLE",
-      description: "Una astuta anciana que resuelve misterios en su pequeño pueblo.",
-      is_discarded: false
-    },
-    {
-      id: crypto.randomUUID(),
-      card_id: crypto.randomUUID(),
-      match_id,
-      player_id,
-      name: "SATTERTHWAITE",
-      description: "Un hombre modesto con una habilidad sorprendente para resolver crímenes.",
-      is_discarded: false
-    },
-    {
-      id: crypto.randomUUID(),
-      card_id: crypto.randomUUID(),
-      match_id,
-      player_id,
-      name: "PYNE",
-      description: "Un detective privado con un enfoque pragmático para resolver casos.",
-      is_discarded: false
-    },
-    {
-      id: crypto.randomUUID(),
-      card_id: crypto.randomUUID(),
-      match_id,
-      player_id,
-      name: "BRENT",
-      description: "Un detective aficionado con un talento natural para la observación.",
-      is_discarded: false,
-      
-    },
-    {
-      id: crypto.randomUUID(),
-      card_id: crypto.randomUUID(),
-      match_id,
-      player_id,
-      name: "TOMMY",
-      description: "Un joven detective que trabaja junto a su esposa Tuppence.",
-      is_discarded: false
-    },
-  ]
-
-  const partialHand = [fullHand[0], null, fullHand[1], null, fullHand[2], fullHand[3]]; // 4 cartas, 2 espacios vacíos en medio
-
-  const emptyHand = [null, null, null, null, null, null]; // 0 cartas, 6 espacios vacíos
-
   const mockOnSelect = vi.fn();
   const mockIsSelected = vi.fn().mockReturnValue(false);
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  })
 
   describe("Rendering", () => {
     it("renders a full hand of cards", () => {
@@ -143,9 +147,5 @@ describe("Hand", () => {
         expect(card.className).not.toContain("ring-4 ring-blue-200");
       });
     });
-  })
-
-  beforeEach(() => {
-    vi.clearAllMocks();
   })
 })
