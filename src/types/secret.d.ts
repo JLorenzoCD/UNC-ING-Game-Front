ced3b2ae3c76836d6d1fe18bd4c2cf055d7940cb
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-09-25 09:25:33 -0300

    ING-47 add `is_revealed` field and fix `SecretType` values casing

--- src/types/secret.d.ts
+++ src/types/secret.d.ts
@@ -1,6 +1,6 @@
 import type { UUID } from './common'
 
-export type SecretType = 'accomplice' | 'innocent' | 'murderer'
+export type SecretType = "ACCOMPLICE" | "INNOCENT" | "MURDERER"
 
 export interface Secret {
 	/**
@@ -46,4 +46,10 @@ export interface MatchSecret {
 	 * @example "550e8400-e29b-41d4-a716-446655440000"
 	 */
 	player_id: UUID | null
+
+	/**
+	 * Indica si el secreto ha sido revelado
+	 * @example true
+	 */
+	is_revealed: boolean
 }
