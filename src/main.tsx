53a9b478a179fdbaf12c8c417d0d1a2073bf2bc9
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-09-27 15:24:58 -0300

    ING-57 add `/match/:matchId/game` route which contains business logic for in-game activity

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import { PlayerProvider } from './contexts/PlayerContext.tsx'
 import './index.css'
 import App from './App.tsx' 
 
+import GameLayout from './containers/game/layout.tsx'
+import GameContainer from './containers/game/index.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HttpServiceProvider>
@@ -16,7 +19,11 @@ createRoot(document.getElementById('root')!).render(
         <PlayerProvider>
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<App />} />              
+              <Route path="/" element={<App />} />
+              
+              <Route element={<GameLayout />}>
+                <Route path="/match/:matchId/game" element={<GameContainer />} />
+              </Route>
             </Routes>
           </BrowserRouter>
         </PlayerProvider>
