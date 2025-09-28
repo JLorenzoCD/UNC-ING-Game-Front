76c35f3dff26e5a248b126c19d30990fddfa55b3
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-09-28 12:23:49 -0300

    ING-57 misc export syntax improvements

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import type { ButtonHTMLAttributes } from 'react'
 
-function Button({ className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
+export default function Button({ className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>) {
 	return (
 		<button
 			className={`${className} focus:outline-none text-white bg-[#2C2C2C] hover:bg-[#4D4D4D] focus:ring-4 focus:ring-[#4D4D4D] font-medium rounded-lg text-sm px-5 py-2.5 disabled:bg-[#6e6e6e]`}
@@ -8,5 +8,3 @@ function Button({ className, ...rest }: ButtonHTMLAttributes<HTMLButtonElement>)
 		/>
 	)
 }
-
-export default Button
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
 import type { InputHTMLAttributes } from 'react'
 
-function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
+export default function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
 	return (
 		<input
 			className={`${className} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400`}
@@ -8,5 +8,3 @@ function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
 		/>
 	)
 }
-
-export default Input
