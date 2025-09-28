84f34b26b4e9a4208627274221650cacaa9175b4
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-09-28 12:22:41 -0300

    ING-57 use frontend paths object to define game page route

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import { HttpServiceProvider } from './contexts/HttpServiceContext.tsx'
 import { WebSocketServiceProvider } from './contexts/WebSocketServiceContext.tsx'
 import { PlayerProvider } from './contexts/PlayerContext.tsx'
+import { FRONTEND_PATHS } from './constants/frontendPaths.ts'
 
 import './index.css'
 import App from './App.tsx' 
@@ -22,7 +23,7 @@ createRoot(document.getElementById('root')!).render(
                 <Route path="/" element={<App />} />
                 
                 <Route element={<GameLayout />}>
-                  <Route path="/match/:matchId/game" element={<GameContainer />} />
+                  <Route path={FRONTEND_PATHS.MATCH_GAME(":matchId")} element={<GameContainer />} />
                 </Route>
               </Routes>
             </BrowserRouter>
