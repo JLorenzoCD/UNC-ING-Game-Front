96605a295e6a9111a96ca1bacece93506bf3d03f
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-10 12:37:11 -0300

    ING-68 add test for DiscardPile

--- src/containers/game/components/DiscardPile.test.tsx
+++ src/containers/game/components/DiscardPile.test.tsx
@@ -1,9 +1,11 @@
 import "@testing-library/jest-dom";
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { act, render, screen } from "@testing-library/react";
 
 import type { GameCard } from "@/types/card";
+
 import DiscardPile from "./DiscardPile";
+import userEvent from "@testing-library/user-event";
 
 const testCard: GameCard = {
   id: crypto.randomUUID(),
@@ -18,16 +20,18 @@ const testCard: GameCard = {
     "Un hombre modesto con una habilidad sorprendente para resolver crímenes.",
 };
 
+const mockOnClick = vi.fn();
+
 describe("DiscardPile", () => {
   describe("Rendering", () => {
     it("renders the empty discard pile", () => {
-      render(<DiscardPile topCard={null} />);
+      render(<DiscardPile topCard={null} onClick={mockOnClick} />);
 
       expect(screen.getByText("Discard pile is empty")).toBeInTheDocument();
     });
 
     it("renders the discard pile with cards", () => {
-      render(<DiscardPile topCard={testCard} />);
+      render(<DiscardPile topCard={testCard} onClick={mockOnClick} />);
 
       const cardElement = screen.getByRole("img");
 
@@ -37,5 +41,34 @@ describe("DiscardPile", () => {
         expect.stringContaining("satterthwaite.png"),
       );
     });
+  });
+
+  it("should call the 'onClick' function when clicking DiscardPile", async () => {
+    const { unmount } = render(
+      <DiscardPile topCard={null} onClick={mockOnClick} />,
+    );
+
+    const discardPileEmty = screen.getByTestId("discard-pile");
+    expect(discardPileEmty).toBeInTheDocument();
+
+    await act(async () => {
+      await userEvent.click(discardPileEmty);
+    });
+
+    expect(mockOnClick).toBeCalledTimes(1);
+    mockOnClick.mockClear();
+
+    unmount();
+
+    render(<DiscardPile topCard={testCard} onClick={mockOnClick} />);
+
+    const discardPileWithCard = screen.getByTestId("discard-pile");
+    expect(discardPileWithCard).toBeInTheDocument();
+
+    await act(async () => {
+      await userEvent.click(discardPileWithCard);
+    });
+
+    expect(mockOnClick).toBeCalledTimes(1);
   });
 });
