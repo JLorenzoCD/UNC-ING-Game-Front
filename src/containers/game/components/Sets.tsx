82725c95a3f9c198810f7cf678d64e5981cad5c6
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-11 16:04:07 -0300

    ING-69 update styles in Sets component

--- src/containers/game/components/Sets.tsx
+++ src/containers/game/components/Sets.tsx
@@ -1,7 +1,8 @@
 import Set from "./Set";
 
 import type { MatchSet } from "@/types/set";
-/* 
+
+/*
 const MATCH_ID = "a1b2c3d4-e5f6-7890-1234-567890abcdef";
 const PLAYER_ID = "00000001-0001-0001-0001-000000000001";
 
@@ -39,7 +40,7 @@ const playerSets: MatchSet[] = [
     type: "Two_Beresford",
     player_id: PLAYER_ID,
     match_id: MATCH_ID,
-    quin_play: true,
+    quin_play: false,
   },
   {
     id: "550e8400-e29b-41d4-a716-446655440006",
@@ -56,7 +57,7 @@ interface Props {
 
 export default function Sets({ sets }: Props) {
   return (
-    <article className="flex gap-5 flex-wrap p-5 justify-center items-center max-w-80">
+    <article className="flex gap-5 flex-wrap p-2 justify-center items-center w-72">
       {sets.map((set) => (
         <Set key={set.id} type={set.type} quin_play={set.quin_play} />
       ))}
