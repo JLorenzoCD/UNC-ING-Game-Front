52b90b17b9ae3f34bead4e671e9566b8cd69b8bf
Tomas Castrillon <tomas.castrillon@mi.unc.edu.ar>
2025-10-20 12:41:41 -0300

    ING-111 correction bug

--- src/contexts/GameContext.tsx
+++ src/contexts/GameContext.tsx
@@ -155,6 +155,12 @@ export default function GameContextProvider({
         payload.updated_match_cards &&
         payload.updated_match_cards.length > 0
       ) {
+        if (payload.type === "LOOK INTO THE ASHES") {
+          const cardsToUpdateList: GameCard[] = [];
+          cardsToUpdateList.push(payload.discarded_card_event);
+          cardsToUpdateList.push(...payload.updated_match_cards);
+          handleUpdateCards(cardsToUpdateList);
+        }
         setCards((current) => {
           const updatedCards = [...current];
 
@@ -166,7 +172,7 @@ export default function GameContextProvider({
             );
             if (eventCardIndex !== -1) {
               updatedCards[eventCardIndex] = payload.discarded_card_event;
-            } // <-- Aquí se eliminó el ';'
+            }
 
             // 2. Obtener las cartas del mazo regular (sin dueño, no descartadas)
             const regularDeckCards = updatedCards.filter(
@@ -203,25 +209,26 @@ export default function GameContextProvider({
             return finalCards;
           }
 
-          if (payload.updated_match_cards) {
-            payload.updated_match_cards.forEach((newCard) => {
-              const index = updatedCards.findIndex((c) => c.id === newCard.id);
-              if (index !== -1) {
-                updatedCards[index] = newCard;
-              }
-            });
-          }
+          // if (payload.updated_match_cards) {
+          //   payload.updated_match_cards.forEach((newCard) => {
+          //     const index = updatedCards.findIndex((c) => c.id === newCard.id);
+          //     if (index !== -1) {
+          //       updatedCards[index] = newCard;
+          //     }
+          //   });
+          // }
 
-          // 8. AÑADIR: Actualizar también la carta de evento que se descartó
-          // (Esto es lo que descarta "LOOK INTO THE ASHES" de la mano del jugador)
-          if (payload.discarded_card_event) {
-            const index = updatedCards.findIndex(
-              (c) => c.id === payload.discarded_card_event.id,
-            );
-            if (index !== -1) {
-              updatedCards[index] = payload.discarded_card_event;
-            }
-          }
+          // // 8. AÑADIR: Actualizar también la carta de evento que se descartó
+          // // (Esto es lo que descarta "LOOK INTO THE ASHES" de la mano del jugador)
+          // if (payload.discarded_card_event) {
+          //   const index = updatedCards.findIndex(
+          //     (c) => c.id === payload.discarded_card_event.id,
+          //   );
+          //   if (index !== -1) {
+          //     updatedCards[index] = payload.discarded_card_event;
+          //   }
+          // }
+
           return updatedCards;
         });
       }
