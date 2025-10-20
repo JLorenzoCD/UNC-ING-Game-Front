4130071633cd2bd3d6e1b4290eee19cd07255174
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-20 15:03:00 -0300

    ING-127 the set's crown changes color depending on whether it is made up of 1 or 2 wild cards

--- src/containers/game/components/Sets.tsx
+++ src/containers/game/components/Sets.tsx
@@ -10,7 +10,12 @@ export default function Sets({ sets }: Props) {
   return (
     <article className="flex gap-5 flex-wrap p-2 justify-center items-center w-72">
       {sets.map((set) => (
-        <Set key={set.id} type={set.type} quin_play={set.quin_play} />
+        <Set
+          key={set.id}
+          type={set.type}
+          quin_play={set.quin_play}
+          quin_count={set.quin_count}
+        />
       ))}
     </article>
   );
