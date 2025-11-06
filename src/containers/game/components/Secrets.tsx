12a6b6f84de3f6aa0d5527b147bce968d5fd0592
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-11-05 23:22:41 -0300

    ING-104 (wip) carousel for sets and secrets

--- src/containers/game/components/Secrets.tsx
+++ src/containers/game/components/Secrets.tsx
@@ -3,6 +3,11 @@ import Secret from "./Secret";
 import type { GameSecret } from "@/types/secret";
 import type { GamePlayer } from "@/types/player";
 import type { MatchSet } from "@/types/set";
+import { useState } from "react";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "@remixicon/react";
+import { twMerge } from "tailwind-merge";
+
+const MAX_SECRETS_DISPLAYED = 3;
 
 type SecretsProps = {
   onSelectTargetEvent?: (target: GamePlayer | GameSecret | MatchSet) => void;
@@ -23,18 +28,67 @@ export default function Secrets({
   target = null,
   isTargetSecret = false,
 }: SecretsProps) {
+  const [page, setPage] = useState<number>(0);
+
+  const startIndex = page * MAX_SECRETS_DISPLAYED;
+  const endIndex = startIndex + MAX_SECRETS_DISPLAYED;
+  const displayedSecrets = secrets.slice(startIndex, endIndex);
+
+  const canGoPrevPage = page > 0;
+  const canGoNextPage = endIndex < secrets.length;
+
+  const handlePrevPage = () => {
+    if (canGoPrevPage) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (canGoNextPage) {
+      setPage(page + 1);
+    }
+  };
+
+  if (secrets.length === 0) {
+    return null;
+  }
+
   return (
-    <div data-testid="secrets" className="flex space-x-4 justify-center">
-      {secrets.map((secret) => (
-        <Secret
-          key={secret.id}
-          onSelectTargetEvent={onSelectTargetEvent}
-          isSelectableSecret={isSelectableSecret}
-          secret={secret}
-          isTargetSecret={isTargetSecret}
-          target={target}
-        />
-      ))}
+    <div data-testid="secrets" className="flex gap-x-0.5 items-center">
+      <button
+        type="button"
+        onClick={handlePrevPage}
+        disabled={!canGoPrevPage}
+        className={twMerge(
+          "text-white disabled:opacity-50 disabled:cursor-not-allowed",
+        )}
+      >
+        <RiArrowLeftSLine />
+      </button>
+
+      <div className="flex gap-x-2 justify-center items-center">
+        {displayedSecrets.map((secret) => (
+          <Secret
+            key={secret.id}
+            onSelectTargetEvent={onSelectTargetEvent}
+            isSelectableSecret={isSelectableSecret}
+            secret={secret}
+            isTargetSecret={isTargetSecret}
+            target={target}
+          />
+        ))}
+      </div>
+
+      <button
+        type="button"
+        onClick={handleNextPage}
+        disabled={!canGoNextPage}
+        className={twMerge(
+          "text-white disabled:opacity-50 disabled:cursor-not-allowed",
+        )}
+      >
+        <RiArrowRightSLine />
+      </button>
     </div>
   );
 }
--- src/containers/game/components/Sets.tsx
+++ src/containers/game/components/Sets.tsx
@@ -3,6 +3,14 @@ import Set from "./Set";
 import type { MatchSet } from "@/types/set";
 import type { GamePlayer } from "@/types/player";
 import type { GameSecret } from "@/types/secret";
+import { useMemo, useState } from "react";
+import {
+  RiArrowLeftSLine,
+  RiArrowRightSLine,
+  RiCloseLargeFill,
+} from "@remixicon/react";
+
+const MAX_SETS_DISPLAYED = 3;
 
 interface Props {
   sets: MatchSet[];
@@ -19,21 +27,96 @@ export default function Sets({
   isTargetSet,
   target,
 }: Props) {
+  const [page, setPage] = useState<number>(0);
+
+  const startIndex = page * MAX_SETS_DISPLAYED;
+  const endIndex = startIndex + MAX_SETS_DISPLAYED;
+
+  const displayedSets: Array<MatchSet | null> = useMemo(() => {
+    let slicedSets = sets.slice(startIndex, endIndex);
+
+    if (slicedSets.length < MAX_SETS_DISPLAYED) {
+      slicedSets = [
+        ...slicedSets,
+        ...Array(MAX_SETS_DISPLAYED - slicedSets.length).fill(null),
+      ];
+    }
+
+    return slicedSets;
+  }, [sets, startIndex, endIndex]);
+
+  const canGoPrevPage = page > 0;
+  const canGoNextPage = endIndex < sets.length;
+
+  const handlePrevPage = () => {
+    if (canGoPrevPage) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (canGoNextPage) {
+      setPage(page + 1);
+    }
+  };
+
+  if (sets.length === 0) {
+    return null;
+  }
+
   return (
-    <article className="flex gap-5 flex-wrap p-2 justify-center items-center w-72">
-      {sets.map((set) => (
-        <Set
-          key={set.id}
-          type={set.type}
-          quin_play={set.quin_play}
-          quin_count={set.quin_count}
-          set_object={set}
-          onSelectTargetEvent={onSelectTargetEvent}
-          isSelectableSet={isSelectableSet}
-          isTargetSet={isTargetSet}
-          target={target}
-        />
-      ))}
+    <article
+      data-testid="secrets"
+      className="flex gap-x-0.5 justify-center items-center"
+    >
+      <button
+        type="button"
+        onClick={handlePrevPage}
+        disabled={!canGoPrevPage}
+        className="text-white disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        <RiArrowLeftSLine />
+      </button>
+
+      <div className="flex gap-x-2 justify-center items-center">
+        {displayedSets.map((set) => {
+          if (!set) {
+            return (
+              <div
+                key={crypto.randomUUID()}
+                // Usamos el mismo tamaño que una Set
+                // para evitar que el layout cambie
+                className="w-15 h-22.5 flex items-center justify-center border-2 border-dashed border-gray-400 text-gray-400 rounded-lg"
+              >
+                <RiCloseLargeFill />
+              </div>
+            );
+          }
+
+          return (
+            <Set
+              key={set.id}
+              type={set.type}
+              quin_play={set.quin_play}
+              quin_count={set.quin_count}
+              set_object={set}
+              onSelectTargetEvent={onSelectTargetEvent}
+              isSelectableSet={isSelectableSet}
+              isTargetSet={isTargetSet}
+              target={target}
+            />
+          );
+        })}
+      </div>
+
+      <button
+        type="button"
+        onClick={handleNextPage}
+        disabled={!canGoNextPage}
+        className="text-white disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        <RiArrowRightSLine />
+      </button>
     </article>
   );
 }
