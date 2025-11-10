0090cb6eadb094408b2d8dfa463c950997172b6b
Ticiano Morvan <ticiano.morvan@gmail.com>
2025-11-10 18:36:00 -0300

    ING-157 add missing field

--- src/containers/game/GameContainer.test.tsx
+++ src/containers/game/GameContainer.test.tsx
@@ -563,6 +563,7 @@ describe("GameContainer", () => {
       isSetEventSelectSetButtonDisabled: false,
       playSet: mockPlaySet,
       setTargetSet: mockSetTargetSet,
+      playStolenSet: vi.fn(),
       executeSetActionToTarget: mockExecuteSetActionToTarget,
       executeFinishTurnSetEvent: mockExecuteFinishTurnSetEvent,
       isPlayerSelectableForSetEvent: vi.fn(),
