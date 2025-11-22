7debfdcb1b8ce55188721b516fbacaa53d57f63d
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-11-22 15:52:39 -0300

    ING-180 component Logs call useBasicGame hook to get logs

--- src/containers/game/GameContainer.tsx
+++ src/containers/game/GameContainer.tsx
@@ -1197,7 +1197,7 @@ export default function GameContainer() {
         data-testid="game-container"
         className="h-screen overflow-y-hidden relative bg-[url('/src/assets/background.png')] bg-cover bg-center"
       >
-        <Logs logs={logs} />
+        <Logs />
 
         {/* Formamos una grilla de 3x3 para posicionar los elementos de la partida. */}
         <div className="h-full w-full grid grid-cols-3 grid-rows-3">
